59341d90d1bb87aeb0f3829517871cb8ff273a50
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-15 22:37:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,8 @@
 class Student
   def hello
-    "Hey there! I' so excited to learn stuff."
+    p "Hey there! I' so excited to learn stuff."
   end
   def raise_hand
-    "Pick me!"
+    [ ]"Pick me!"
   end
 end
